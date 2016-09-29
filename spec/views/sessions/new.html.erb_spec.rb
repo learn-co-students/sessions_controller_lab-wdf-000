71d272bd1ec3d70d84a405cb1777ca6139dcc337
Natalisp <nathalipat@gmail.com>
2016-09-28 20:59:02 -0400

    Done.

---
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# describe 'sessions/new' do 
+#   it 'users can enter their name in a form and click login' do 
+#     assign :name, "Nat"
+#     render 
+
+#     expect(rendered).to have_text('Hi, Nat.')
+#   end
+# end
